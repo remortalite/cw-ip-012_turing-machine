0ec5f1419495227c9ee87baa352e4ed40f715544
Ignat Mora <morrig407@gmail.com>
2021-04-30 14:07:14 +0700

    Add describe for get_word()

--- src/libturing/process_file.h
+++ src/libturing/process_file.h
@@ -9,7 +9,12 @@ char* get_line(FILE*);
 
 char* strip(char*);
 
-char* get_word(char*, char**); // allocate memory for new word
+// get_word:
+// strip `line` using strip()
+// allocate memory for new word
+// save in `ret` pointer to next symbol after `word` (rest of the line)
+// return: word or NULL
+char* get_word(char* line, char** ret);
 
 Program fill_program(FILE*, Program);
 
